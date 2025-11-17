b31d151fb4df28309d865479eccd7f3e87605b49
mohamed <khaldimohamed82@gmail.com>
2025-11-17 18:00:01 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ cub.h
@@ -0,0 +1,33 @@
+#ifndef CUB_H
+#define CUB_H
+
+#include "./libft/libft.h"
+#include "./mlx/mlx.h"
+#include <fcntl.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct s_textuers {
+  char *path;
+} t_textuers;
+
+typedef struct s_map {
+  char **grid;
+  int width;
+  int height;
+} t_map;
+
+typedef struct s_config {
+  t_textuers text[4];
+  int f_color;
+  int cel_color;
+  int player_x;
+  int player_y;
+  int player_dir;
+} t_config;
+
+#endif
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <cub.h>
+#include <stdio.h>
+
+int main(int ac, char **av) {
+  if (ac != 2) {
+    /* TODO:  write the error func */
+    // ft_error(ac);
+    perror("Error: argummment not valid");
+  }
+  /* TODO:  ft_pacre */
+  // ft_parce(av);
+  return (0);
+}
